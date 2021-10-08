a07658f05345d83ea651cfc611af466a3162822a
Yurii Hrecheniuk <yuriy@hrecheni.uk>
2021-10-08 20:32:56 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const enqueueForm = document.getElementById('enqueue-form');
+const inputField = enqueueForm.querySelector('#input-field');
+
+const dequeueBtn = document.getElementById('dequeue-btn');
+
+const queueView = document.getElementById('queue-view');
+
+const message = document.getElementById('message');
+
+let localQueue;
+
+if (!localStorage.getItem('queue')) {
+  localStorage.setItem('queue', '')
+  localQueue = [];
+
+  message.hidden = false;
+} else {
+  localQueue = localStorage.getItem('queue').split(' ');
+
+  for (let item of localQueue) {
+    enqueue(item);
+  }
+}
+
+enqueueForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const contents = inputField.value;
+
+  enqueue(contents);
+  localQueue.push(contents);
+
+  message.hidden = true;
+  inputField.value = '';
+  dequeueBtn.disabled = false;
+});
+
+dequeueBtn.addEventListener('click', dequeue);
+dequeueBtn.addEventListener('click', () => {
+  if (!queueView.firstChild)
+    message.hidden = false;
+})
+
+// when user leaves the page we save our queue in local storage
+window.addEventListener('unload', () => {
+  localStorage.setItem('queue', localQueue.join(' '));
+})
+
+function enqueue(contents) {
+  if (contents) {
+    queueView.appendChild(queueItem(contents));
+  }
+}
+
+function dequeue() {
+  if (queueView.firstChild)
+    queueView.removeChild(queueView.firstChild);
+  if (!queueView.firstChild)
+    dequeueBtn.disabled = true;
+
+  localQueue.shift();
+}
+
+function queueItem(contents) {
+  const queueItem = document.createElement('div');
+
+  queueItem.className = 'queue-item';
+  queueItem.innerText = contents;
+
+  return queueItem;
+}
